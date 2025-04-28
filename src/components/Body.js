961d40df21da030965d0c0a9677294a56d7e805d
Vishal Patel <94670096+vvishalpatel@users.noreply.github.com>
2025-04-28 14:27:24 +0530

    Remove back-end code

--- src/components/Body.js
+++ src/components/Body.js
@@ -40,7 +40,7 @@ const Body = () => {
         const lng = process.env.REACT_APP_SWIGGY_LNG;
 
     
-        const data = await fetch("https://swaad-food-reactjs-cfi9.vercel.app/")
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=27.952862214938285&lng=80.7915697619319&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
         const json = await data.json();
         console.log(
             json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
